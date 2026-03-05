2abe5ca1fbbb54ed1d05b1431d723276b20ddd9e
Arslan Kutlubaev <kutlubaev99@gmail.com>
2026-03-05 16:46:28 +0500

    добавление главной страницы с возможностью выбора нужного метода

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace Lab_rab_3_KutlubaevAD_БПИ_23_02.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
